65ddd2f8240f4e79c86ff6ccbae01c4aea6b1134
lichomonjelat <lichomonjelat@gmail.com>
2021-06-06 11:08:17 -0300

    checkout terminado

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import { FormControl, TextField } from '@material-ui/core'
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import './Checkout.scss'
 import db from '../../db'
@@ -9,11 +9,29 @@ export default function Checkout() {
     const { cart } = useContext(CartContext)
 
     const [id, setID] = useState()
+    const [customer, setCustomer] = useState()
     const [orderEnd, setOrderEnd] = useState(false)
     const { register, handleSubmit } = useForm();
 
+    const [qty, setQty] = useState(0)
+
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        let cantidad = cart.reduce(function (total, currentValue) {
+            return total + currentValue.item.count
+        }, 0);
+        setQty(cantidad);
+
+        let total = cart.reduce(function(total,currentValue){
+            return total + (currentValue.item.data.product_price*currentValue.item.count)
+        }, 0);
+        setTotal(total);
+    }, [cart]);
+
     function onSubmit(data) {
-        /*setOrderEnd(true);
+        setCustomer(data.name);
+        setOrderEnd(true);
         db.collection("orders").add({
             order:{
                 buyer:{
@@ -24,16 +42,16 @@ export default function Checkout() {
                 },
                 items:{cart},
                 total:{
-                    total:420
+                    total
                 }
             }
         }).then(function (docRef) {
-            setID(docRef)
+            setID(docRef.id)
         })
             .catch(function (error) {
                 console.error("Error adding document: ", error);
-            });*/
-        console.log(data)
+            });
+        console.log(cart)
 
     }
     return (
@@ -42,7 +60,7 @@ export default function Checkout() {
             {
                 orderEnd ?
                     <div>
-                        <h2>Juancho</h2>
+                        <h2>{customer}</h2>
                         <h3>Su orden ha sido creada con exito! Su numero de orden es:</h3>
                         <h3>{id}</h3>
                         <h3>Gracias por confiar en Flipper Gadgets!</h3>
@@ -51,7 +69,7 @@ export default function Checkout() {
                         <div className="container mt-5 ">
                             <FormControl fullWidth variant="outlined">
                                 <TextField
-                                    ref={register}
+                                    {...register("name")}
                                     id="outlined-full-width"
                                     label="Nombre"
                                     style={{ margin: 8 }}
@@ -65,7 +83,7 @@ export default function Checkout() {
                             </FormControl>
                             <FormControl fullWidth variant="outlined">
                                 <TextField
-                                    ref={register}
+                                    {...register("lastname")}
                                     id="outlined-full-width"
                                     label="Apellido"
                                     style={{ margin: 8 }}
@@ -80,11 +98,25 @@ export default function Checkout() {
                             </FormControl>
                             <FormControl fullWidth variant="outlined">
                                 <TextField
-                                    ref={register}
+                                    {...register("email")}
                                     id="outlined-full-width"
                                     label="Email"
                                     style={{ margin: 8 }}
                                     placeholder="Ingrese su email"
+                                    margin="normal"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="outlined"
+                                />
+                            </FormControl>
+                            <FormControl fullWidth variant="outlined">
+                                <TextField
+                                    {...register("phone")}
+                                    id="outlined-full-width"
+                                    label="Telefono"
+                                    style={{ margin: 8 }}
+                                    placeholder="Ingrese su telefono"
                                     margin="normal"
                                     InputLabelProps={{
                                         shrink: true,
